f18ca5fafc42365f9fa71f85f6a4be8ce8ccca78
Vasko Jovanoski <vacikabre@hotmail.com>
2020-02-09 14:34:52 +0100

    Initial commit

---
+++ src/main/kotlin/com/project/najdiprevoz/NajdiprevozApplication.kt
@@ -0,0 +1,11 @@
+package com.project.najdiprevoz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NajdiprevozApplication
+
+fun main(args: Array<String>) {
+	runApplication<NajdiprevozApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/najdiprevoz/NajdiprevozApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.najdiprevoz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NajdiprevozApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
