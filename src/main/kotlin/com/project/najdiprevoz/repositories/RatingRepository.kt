380b7496e720eb426ffc4a3e7799ce3ef8baa117
Vasko Jovanoski <vacikabre@hotmail.com>
2020-02-12 20:28:43 +0100

    Rating & ride repository implemented

--- src/main/kotlin/com/project/najdiprevoz/repositories/RatingRepository.kt
+++ src/main/kotlin/com/project/najdiprevoz/repositories/RatingRepository.kt
@@ -1,8 +1,14 @@
 package com.project.najdiprevoz.repositories
 
+import com.project.najdiprevoz.domain.Member
 import com.project.najdiprevoz.domain.Rating
+import com.project.najdiprevoz.domain.Ride
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RatingRepository : JpaRepository<Rating, Long>
+interface RatingRepository : JpaRepository<Rating, Long> {
+    fun findRatingsByRide(ride: Ride): List<Rating>?
+
+    fun findRatingsByAuthor(author: Member): List<Rating>?
+}
