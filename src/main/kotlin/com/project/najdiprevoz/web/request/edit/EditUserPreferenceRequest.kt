a0e6f9e3c13f10e0696b16c481953dd9f3866c3c
Vasko Jovanoski <vacikabre@hotmail.com>
2020-02-22 22:14:32 +0100

    UserPreference delete, properties moved to ride entity

--- src/main/kotlin/com/project/najdiprevoz/web/request/edit/EditUserPreferenceRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.project.najdiprevoz.web.request.edit
-
-class EditUserPreferenceRequest(
-        val isSmokingAllowed: Boolean,
-        val isPetAllowed: Boolean,
-        val userId: Long
-)
