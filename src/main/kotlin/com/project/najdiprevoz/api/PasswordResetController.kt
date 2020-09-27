2216ca1c841b776496a596bef390597ecee5eb9c
Vasko Jovanoski <vacikabre@hotmail.com>
2020-09-27 23:44:34 +0200

    few back-end optimizations

--- src/main/kotlin/com/project/najdiprevoz/api/PasswordResetController.kt
+++ src/main/kotlin/com/project/najdiprevoz/api/PasswordResetController.kt
@@ -1,10 +1,6 @@
 package com.project.najdiprevoz.api
-import com.project.najdiprevoz.domain.User
-import com.project.najdiprevoz.exceptions.TokenNotValidOrExpiredException
-import com.project.najdiprevoz.repositories.PasswordResetTokenRepository
-import com.project.najdiprevoz.services.UserService
+import com.project.najdiprevoz.services.PasswordForgotService
 import org.springframework.stereotype.Controller
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,26 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/api/reset-password")
-class PasswordResetController(private val userService: UserService,
-                              private val tokenRepository: PasswordResetTokenRepository) {
+class PasswordResetController(private val passwordForgotService: PasswordForgotService) {
 
     @GetMapping
-    fun isTokenValid(@RequestParam(required = false) token: String): Boolean {
-        val resetToken = tokenRepository.findByToken(token)
-        if (resetToken != null && !resetToken.isExpired()) {
-            return true
-        }
-        else {
-            throw TokenNotValidOrExpiredException("Token was either invalid or expired. Token: $token")
-        }
-    }
+    fun isTokenValid(@RequestParam(required = false) token: String): Boolean  =
+            passwordForgotService.isTokenValid(token)
 
     @PostMapping
-    @Transactional
-    fun handlePasswordReset(tokenSent: String, newPassword: String) {
-        val token = tokenRepository.findByToken(tokenSent)
-        val user: User = token!!.user
-        userService.updatePassword(newPassword, user.id)
-        tokenRepository.delete(token)
-    }
+    fun handlePasswordReset(tokenSent: String, newPassword: String) =
+            passwordForgotService.handlePasswordReset(tokenSent, newPassword)
 }
---
+++ src/main/kotlin/com/project/najdiprevoz/services/PasswordForgotService.kt
@@ -0,0 +1,57 @@
+package com.project.najdiprevoz.services
+
+import com.project.najdiprevoz.domain.Mail
+import com.project.najdiprevoz.domain.PasswordResetToken
+import com.project.najdiprevoz.domain.User
+import com.project.najdiprevoz.exceptions.TokenNotValidOrExpiredException
+import com.project.najdiprevoz.repositories.PasswordResetTokenRepository
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+class PasswordForgotService(private val userService: UserService,
+                            private val tokenRepository: PasswordResetTokenRepository,
+                            private val emailService: EmailService,
+                            @Value("najdiprevoz.official-app-link") private val officialAppUrl: String) {
+
+    @Transactional
+    fun createResetTokenForUser(username: String) {
+        val user: User? = userService.findUserByUsername(username)
+        val token = PasswordResetToken(
+                token = UUID.randomUUID().toString(),
+                user = user!!)
+        token.setExpiryDate(30)
+        tokenRepository.save(token)
+        val mail = Mail()
+        mail.from = "no-reply@najdiprevoz.com.mk"
+        mail.to = username
+        mail.subject = "Password reset request"
+        val model: MutableMap<String, Any> = HashMap()
+        model["token"] = token
+        model["user"] = user
+        //TODO: Move this to application.yml
+        model["signature"] = officialAppUrl
+        model["resetUrl"] = officialAppUrl + "/reset-password?token=" + token.token
+        mail.model = model
+        emailService.sendEmail(mail)
+    }
+
+    fun isTokenValid(token: String): Boolean {
+        val resetToken = tokenRepository.findByToken(token)
+        if (resetToken != null && !resetToken.isExpired()) {
+            return true
+        } else {
+            throw TokenNotValidOrExpiredException("Token was either invalid or expired. Token: $token")
+        }
+    }
+
+    @Transactional
+    fun handlePasswordReset(tokenSent: String, newPassword: String) {
+        val token = tokenRepository.findByToken(tokenSent)
+        val user: User = token!!.user
+        userService.updatePassword(newPassword, user.id)
+        tokenRepository.delete(token)
+    }
+}
