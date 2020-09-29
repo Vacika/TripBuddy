14b094a50a7b9adb657d7d53cb432a06ce712486
Vasko Jovanoski <vasko.jovanoski@sorsix.com>
2020-09-29 22:38:31 +0200

    Fix mail sending, ordered structure by language for templates

--- src/main/kotlin/com/project/najdiprevoz/services/EmailService.kt
+++ src/main/kotlin/com/project/najdiprevoz/services/EmailService.kt
@@ -1,6 +1,8 @@
 package com.project.najdiprevoz.services
 
 import com.project.najdiprevoz.domain.Mail
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.context.Context;
@@ -14,22 +16,26 @@ import java.nio.charset.StandardCharsets
 @Service
 class EmailService(private val emailSender: JavaMailSender,
                    private val templateEngine: SpringTemplateEngine) {
+    val logger: Logger = LoggerFactory.getLogger(EmailService::class.java)
 
     fun sendForgetPasswordMail(mail: Mail) {
         try {
+            logger.debug("[EMAIL SERVICE] Sending [Forget Password] Mail to ${mail.to}")
             val message: MimeMessage = emailSender.createMimeMessage()
             val helper = MimeMessageHelper(message,
                     MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                     StandardCharsets.UTF_8.name())
             val context = Context()
             context.setVariables(mail.model)
-            val html: String = templateEngine.process("forget-password-template-${mail.lang.toUpperCase()}", context)
+            val html: String = templateEngine.process("${mail.lang.toLowerCase()}/forget-password-template", context)
             helper.setTo(mail.to)
             helper.setText(html, true)
             helper.setSubject(mail.subject)
             helper.setFrom(mail.from)
             emailSender.send(message)
+            logger.debug("[EMAIL-SERVICE] Mail sent successfully!")
         } catch (e: Exception) {
+            logger.error("Mail for ${mail.to} was not sent!")
             throw RuntimeException(e)
         }
     }
